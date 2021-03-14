c085d62b859cf7871d9c14ed57a9674b8be9d612
hina.k <nahhi125@i.softbank.jp>
2021-03-14 07:49:49 +0000

    kadai-3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,14 +4,11 @@ class TasksController < ApplicationController
   
   def index
      if logged_in?
-    #  @user = current_user
-    #  @task = current_user.tasks.build
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
      end
   end
 
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -31,11 +28,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-   # @task = Task.find(params[:id])
   end
 
   def update
-  #  @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  #before_action :correct_user, only: [:show]
-
-  def show
-   @user = User.find(params[:id])
-   @tasks = @user.tasks.order('created_at DESC').page(params[:page])
-  #  counts(@user)
-  end
+  before_action :require_user_logged_in, only: [:index]
   
   def new
     @user = User.new
